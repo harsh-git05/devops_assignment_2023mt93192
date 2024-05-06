c8f067484be63dfa277df3d4f644028e2fe44af8
Harsh <harsh@Harshs-MacBook-Air.local>
2024-05-06 11:07:02 +0530

    Commit for the demo

---
+++ demo2/data.js
@@ -0,0 +1,11 @@
+const Juser= {
+    name:'harsh',
+    age: 18,
+    location:'rajasthan',
+    email: "harsh@google.com",
+    isloggedin : false,
+    lastlogindays:['monday','wednesday']
+}
+
+console.log(Juser.email)
+console.log(Juser['email'])
---
+++ demo2/info.js
@@ -0,0 +1,15 @@
+console.log("hi harsh")
+const accountID=133434
+let accountemail = 'abcd@gmail.com'
+var accountpassword = '123445'
+accountcity = 'jaipur'
+
+let accountstate;
+
+accountemail= 'shfhjsh@gmail.com'
+accountpassword = '234231'
+accountcity='rajasthan'
+
+console.log(accountID)
+console.table([accountemail,accountpassword,accountcity,accountstate])
+// accountID=2
