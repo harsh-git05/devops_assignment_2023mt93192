a14078d570b04b19dbdfd5ebbab3d867bbad1bac
harsh-git05 <kansaraharsh619@gmail.com>
2024-05-06 11:59:43 +0530

    commit the updated1-demo

--- demo2/data.js
+++ demo2/data.js
@@ -3,4 +3,17 @@ const Juser= {
     age: 18,
     location:'rajasthan',
     email: "harsh@google.com",
+}
+
+let accountstate;
+
+accountemail= 'shfhjsh@gmail.com'
+accountpassword = '234231'
+accountcity='rajasthan'
+
+console.log(accountID)
+console.table([accountemail,accountpassword,accountcity,accountstate])
+// accountID=2
+
+
     
