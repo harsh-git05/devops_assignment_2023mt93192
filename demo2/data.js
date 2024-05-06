f01f70a02c68b776987a2141c64505e8b002c6b4
harsh-git05 <kansaraharsh619@gmail.com>
2024-05-06 11:39:48 +0530

    commit the updated-demo

--- demo2/data.js
+++ demo2/data.js
@@ -3,9 +3,4 @@ const Juser= {
     age: 18,
     location:'rajasthan',
     email: "harsh@google.com",
-    isloggedin : false,
-    lastlogindays:['monday','wednesday']
-}
-
-console.log(Juser.email)
-console.log(Juser['email'])
+    
